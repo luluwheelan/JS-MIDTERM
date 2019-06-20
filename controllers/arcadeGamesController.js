f6e77512545139ea1ab4517193c7da6292b57828
Yingge Hu <ayanamirei629@gmail.com>
2019-06-20 17:45:23 -0400

    controller changed

--- controllers/arcadeGamesController.js
+++ controllers/arcadeGamesController.js
@@ -1,22 +1,103 @@
 const Resource = require('../models/resource');
 const mongoose = require('mongoose');
 
-exports.index = (req, res) => {};
+exports.index = (req, res) => {
+    ArcadeGame.find()
+        .then(arcadeGames => {
+            res.render("arcadeGames/index", {
+                arcadeGames: arcadeGames,
+                title: "arcadeGames"
+            });
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("/");
+        });
+};
+
+exports.show = (req, res) => {
+    ArcadeGame.findById({
+            _id: req.params.id
+        })
+        .then(arcadeGame => {
+            res.render("arcadeGames/show", {
+                title: arcadeGame.fullName,
+                arcadeGames: arcadeGame
+            });
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("/arcadeGames");
+        });
+};
+
+exports.new = (req, res) => {
+    res.render("arcadeGames/new", {
+        title: "Create New arcadeGame"
+    });
+};
+
+exports.edit = (req, res) => {
+    ArcadeGame.findById({
+            _id: req.params.id
+        })
+        .then(arcadeGame => {
+            res.render("arcadeGames/edit", {
+                title: arcadeGame.fullName,
+                arcadeGame: arcadeGame
+            });
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("/arcadeGames");
+        });
+};
+
+exports.create = async (req, res) => {
+    ArcadeGame.create(req.body.arcadeGame)
+        .then(() => {
+            req.flash("success", "Create arcade Game succeed!");
+            res.redirect("/arcadeGames");
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("arcadeGames/new");
+        });
+};
+
+exports.update = (req, res) => {
+    ArcadeGame.updateOne({
+            _id: req.body.id
+        }, req.body.arcadeGames, {
+            runValidators: true
+        })
+        .then(() => {
+            req.flash("success", "Update arcade Game succeed!");
+            res.redirect("/arcadeGames");
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("/arcadeGames");
+        });
+};
 
 
-exports.show = (req, res) => {};
-
-
-exports.new = (req, res) => {};
-
-
-exports.edit = (req, res) => {};
-
-
-exports.create = async (req, res) => {};
-
-
-exports.update = (req, res) => {};
-
-
-exports.destroy = (req, res) => {};
+exports.destroy = (req, res) => {
+    ArcadeGame.deleteOne({
+            _id: req.body.id
+        })
+        .then(() => {
+            req.flash("success", "Delete arcade Game succeed!");
+            res.redirect("/arcadeGames");
+        })
+        .catch(err => {
+            console.log(`ERROR: ${err}`);
+            req.flash("error", `ERROR: ${err}`);
+            res.redirect("/arcadeGames");
+        });
+};
